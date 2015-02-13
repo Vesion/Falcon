18b29b6bacaa8e9d8c48f1e4955aa46b0dd4f076
Shine <xuxiang19930924@126.com>
2015-02-13 11:45:46 +0800

    New package

--- Falcon/Falcon/__init__.py
+++ Falcon/Falcon/__init__.py
@@ -1,2 +1,18 @@
 ﻿# -*- coding: utf-8 -*-
 
+"""
+A handful tool for playing with zhihu.com
+"""
+
+__title__ = 'falcon'
+__version__ = '2.0.1'
+__author__ = 'Shine Xu'
+__license__ = 'The MIT License (MIT)'
+__copyright__ = 'Copyright 2015 Shine Xu'
+
+from .Session import Session
+from .Answer import Answer
+from .Question import Question
+from .Topic import Topic
+from .User import User
+from .Collection import Collection
