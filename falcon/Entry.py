b2b3747e5b6415da175d13030d1ab2609acf79f4
Shine <xuxiang19930924@126.com>
2015-08-15 23:30:57 +0800

    Entry: fix docstring indent

--- falcon/Entry.py
+++ falcon/Entry.py
@@ -16,10 +16,9 @@ class Entry():
     """
     Base class for all specified entries.
     An entry is identified by a unique eid(entry-id), like '/question/27936593'.
-    NOTE:
-        All of get_* APIs in tool classes are NOT getting an entry,
-        but getting text|eid|eidlist|dict, for performance regrading.
-        Use that Entry(session, eid) to manually get an entry content indeed
+    All of get_* APIs in tool classes are NOT getting an entry,
+    but getting text|eid|eidlist|dict, for performance regrading.
+    Use that Entry(session, eid) to manually get an entry content indeed
     API: encode2Character
     Wrapper: getContent, getSoup
     """
